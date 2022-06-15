dc0949b57ec1960ab780a29f2a8ba00c7627626b
Ilya <ku4in.il@yandex.ru>
2022-06-15 14:28:51 +0300

    правки путей, чистка лишних плагинов

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import * as $ from 'jquery'
 
 import WebpackLogo from '@/assets/webpack-logo'
 
-import './styles/styles.css'
-import './styles/styles.scss'
+import '@/styles/styles.css'
+import '@/styles/styles.scss'
 
